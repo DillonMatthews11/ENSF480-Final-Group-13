c82fbcab3d09e668c4e240ae37b0c6c2f9eceeea
DillonMatthews11 <81485381+DillonMatthews11@users.noreply.github.com>
2021-12-04 21:32:38 -0700

    Add files via upload

---
+++ Part@/src/Entities/Manager.java
@@ -0,0 +1,10 @@
+package Resources.Entities;
+
+public class Manager extends Person {
+    //ctor
+    Manager(String first, String last, String emailadd) {
+        this.firstName = first;
+        this.lastName = last;
+        this.email = emailadd;
+    }
+}
---
+++ Part@/src/Entities/Person.java
@@ -0,0 +1,7 @@
+package Resources.Entities;
+
+public abstract class Person {
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+}
